2a429c84e2564cb439f903525eb1daa7be0ae6e1
azarateo <azarate@cool4code.com>
2017-03-03 13:57:06 -0500

    Added Camera Script

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+	public Transform target;
+	Vector3 offset;
+	float smoothing = 5f;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		Vector3 camPosition = target.position + offset;
+		transform.position = Vector3.Lerp(transform.position, camPosition, smoothing * Time.deltaTime);
+	}
+
+}
